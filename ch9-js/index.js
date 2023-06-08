0d0cb0f35c5391d5383710fec9b535e810795d12
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2023-06-08 21:55:36 +0300

    Introduction to JavaScript

---
+++ ch9-js/index.js
@@ -0,0 +1,9 @@
+let x = 5;
+let y = 12;
+
+console.log(x, y, x+y);
+document.write("Hello JavaScript");
+document.write("<br>");
+// alert("Hello CF");
+
+document.write(`Hello Coding Factory ${x+y}`);
